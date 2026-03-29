563bf3047bf141490a88264f9f212cde71d5255b
rohitjangid01 <rohit.devteam@webdaire.com>
2026-03-29 16:22:20 +0530

    routes changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   { path: "auth", loadChildren: () => import("./modules/auth/auth.routes").then(m => m.AUTH_ROUTES) },
   { path: "admin", loadChildren: () => import("./modules/admin/admin.routes").then(m => m.ADMIN_ROUTES) },
-  { path: "", loadChildren: () => import("./modules/pages/pages.routes").then(m => m.PAGES_ROUTES) },
+  { path: "pages", loadChildren: () => import("./modules/pages/pages.routes").then(m => m.PAGES_ROUTES) },
   { path: "**", redirectTo: "" }
 ];
